a7fcd330c02cbaaca3438f719ab2b7c8d3234780
Malygina Svetlana <malygina25@yandex.ru>
2022-10-17 18:54:49 +0300

    3

---
+++ 3/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+
+        int number = Convert.ToInt32(Console.ReadLine());
+
+
+        for (int i = 1; i <= number; i++)
+
+            Console.Write($"{Math.Pow(i, 3)}. ");
+    }
+}
