688b04d13d83edb07cda66c01642297a40c93c81
Malygina Svetlana <malygina25@yandex.ru>
2022-10-17 18:45:31 +0300

    1

---
+++ 1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+    if (number[0] == number[4] || number[1] == number[3]){
+    Console.WriteLine($"Ваше число:{number} - палиндром.");
+    }
+
+    else 
+        Console.WriteLine($"Ваше число: {number} - не палиндром");
+    }
+    if (number!.Length == 5) {
+        CheckingNumber(number);
+    }
+    else 
+    
+        Console.WriteLine($"Введите правильное число");
